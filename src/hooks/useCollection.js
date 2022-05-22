f97e06f5722f0c4f9adbfaca7c0950eea8c13b93
develad <develadb@gmail.com>
2022-05-22 23:57:20 +0300

    lesson 137 - adding useCollection hook

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { projectFirestore } from "../firebase/config";
+
+export const useCollection = (collection) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let ref = projectFirestore.collection(collection);
+
+    // onSnapshot is like a websocket or real time listener that run
+    // every time there is a change in the firebase collection
+    const unsubscribe = ref.onSnapshot(
+      (snapshot) => {
+        let results = [];
+        snapshot.docs.forEach((doc) => {
+          results.push({ ...doc.data(), id: doc.id });
+        });
+
+        //update state
+        setDocuments(results);
+        setError(null);
+      },
+      (error) => {
+        //2nd argument of onSnapshot is the error case function
+        console.log(error);
+        setError("could not fetch the data");
+      }
+    );
+
+    // unsubscribe on unmount
+
+    return () => unsubscribe();
+  }, [collection]);
+
+  return { documents, error };
+};
